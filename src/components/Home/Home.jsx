c99a8c04d2fd6c7bf770f1e1525fa304a3b99acc
Alal Hosen <alalhosen1992620120@gmail.com>
2026-02-09 01:15:57 +0600

    firebase install & setup

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 
 const Home = () => {
     return (
-        <div>
+        <div className="text-3xl">
           This is home.  
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './components/Root/Root.jsx'
 import Home from './components/Home/Home.jsx'
 
@@ -21,7 +20,7 @@ const router=createBrowserRouter([
 
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-  <RouterProvider router={router} />
-  </React.StrictMode>,
+  <StrictMode>
+  <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
 )
