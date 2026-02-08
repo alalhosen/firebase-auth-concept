8d30183838fc7c44f2ab0b3c68b306d27d2fe740
Alal Hosen <alalhosen1992620120@gmail.com>
2026-02-08 23:59:48 +0600

    react router setup

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ const router=createBrowserRouter([
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+  <RouterProvider router={router} />
+  </React.StrictMode>,
 )
