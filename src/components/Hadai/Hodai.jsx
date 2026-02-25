1ed44031249ab9df708ea4767e09c050f3ca8a65
Alal Hosen <alalhosen1992620120@gmail.com>
2026-02-25 22:37:21 +0600

    use useContext & AuthContext

--- src/components/Hadai/Hodai.jsx
+++ src/components/Hadai/Hodai.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const Hodai = () => {
-    return (
-        <div>
-            <h2>hodai</h2>
-        </div>
-    );
+  const user = useContext(AuthContext);
+  console.log(user);
+  return (
+    <div>
+      <h2>hodai</h2>
+    </div>
+  );
 };
 
 export default Hodai;
