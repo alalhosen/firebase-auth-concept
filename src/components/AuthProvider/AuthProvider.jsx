4e30942217b2f67487dd673e6a41a775c4f111af
Alal Hosen <alalhosen1992620120@gmail.com>
2026-03-01 22:09:17 +0600

    register & login setup

--- src/components/AuthProvider/AuthProvider.jsx
+++ src/components/AuthProvider/AuthProvider.jsx
@@ -1,14 +1,24 @@
 import React, { createContext, useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../Firebase/Firebase.init";
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const registerUser = (email, password) => {
-    createUserWithEmailAndPassword(auth, email, password);
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((result) =>
+      console.log(result.user),
+    );
+  };
+  const loginUser = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((result) =>
+      console.log(result.user),
+    );
   };
   const authInfo = {
     registerUser,
+    loginUser,
   };
 
   return (
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,15 +2,16 @@ import { useContext } from "react";
 import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const Login = () => {
-  const user = useContext(AuthContext);
-  console.log(user);
+  const { loginUser } = useContext(AuthContext);
 
   const handleLogin = (e) => {
     e.preventDefault();
+
     const email = e.target.email.value;
     const password = e.target.password.value;
 
     console.log(email, password);
+    loginUser(email, password);
   };
   return (
     <div className="w-[40%] mx-auto min-w[500px] border-red-500 p-2 rounded-xl">
